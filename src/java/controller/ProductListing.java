7613477ae0483ae6fb26b711bdd620b110a4ac35
chamudith bandara <chamudithbuddima@gmail.com>
2024-09-06 16:08:50 +0530

    new

---
+++ src/java/controller/ProductListing.java
@@ -0,0 +1,40 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import model.HibernateUtil;
+import org.hibernate.Session;
+
+@MultipartConfig
+@WebServlet(name = "ProductListing", urlPatterns = {"/ProductListing"})
+public class ProductListing extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String categoryId = request.getParameter("categoryId");
+        String modelId = request.getParameter("modelId");
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        String storageId = request.getParameter("storageId");
+        String colorId = request.getParameter("colorId");
+        String conditionId = request.getParameter("conditionId");
+        String price = request.getParameter("price");
+        String quantity = request.getParameter("quantity");
+
+        Part image1 = request.getPart("image1");
+        Part image2 = request.getPart("image2");
+        Part image3 = request.getPart("image3");
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+    }
+
+}
--- web/assets/js/my-account.js
+++ web/assets/js/my-account.js
@@ -55,5 +55,61 @@ function updateModels() {
         }
     });
 
+}
+
+async function productListing() {
+    const categorySelectTag = document.getElementById("categorySelect");
+    const modelSelectTag = document.getElementById("modelSelect");
+    const titleTag = document.getElementById("title");
+    const descriptionTag = document.getElementById("description");
+    const storageSelectTag = document.getElementById("storageSelect");
+    const colorSelectTag = document.getElementById("colorSelect");
+    const conditionSelectTag = document.getElementById("conditionSelect");
+    const priceTag = document.getElementById("price");
+    const quantityTag = document.getElementById("quantity");
+    const image1Tag = document.getElementById("image1");
+    const image2Tag = document.getElementById("image2");
+    const image3Tag = document.getElementById("image3");
+
+    const data = new FormData();
+    data.append("categoryId", categorySelectTag.value);
+    data.append("modelId", modelSelectTag.value);
+    data.append("title", titleTag.value);
+    data.append("description", descriptionTag.value);
+    data.append("storageId", storageSelectTag.value);
+    data.append("colorId", colorSelectTag.value);
+    data.append("conditionId", conditionSelectTag.value);
+    data.append("price", priceTag.value);
+    data.append("quantity", quantityTag.value);
+    data.append("image1", image1Tag.files[0]);
+    data.append("image2", image2Tag.files[0]);
+    data.append("image3", image3Tag.files[0]);
+
+
+
+    const response = await fetch(
+            "ProductListing",
+            {
+                method: "POST",
+                body: data
+            }
+    );
+
+    if (response.ok) {
+        const json = await response.json();
+
+        console.log(json);
+
+        if (json.success) {
+
+
+        } else {
+
+        }
+
+    } else {
+        document.getElementById("message").innerHTML = "Please try again Later ";
+    }
+
 
 }
